b11c3ae79f0d1747b740ba5a68c5a899724885b8
Nevzat Gunay <ngunay@gmail.com>
2022-10-06 22:47:26 +0100

    amended vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reason/',
+  base: '/reason-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
